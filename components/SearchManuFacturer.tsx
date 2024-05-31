c59349d44a894847cd62295c9f5998239525dec2
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2024-05-31 15:54:34 +0600

    commit

--- components/SearchManuFacturer.tsx
+++ components/SearchManuFacturer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Combobox, Transition } from "@headlessui/react";
 import { useState, Fragment } from "react";
+import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types";
 import Image from "next/image";
 
@@ -9,6 +10,16 @@ const SearchManuFacturer = ({
   setManufacturer,
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturer
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
     <div className="search-manufacturer">
@@ -36,7 +47,16 @@ const SearchManuFacturer = ({
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
-            <Combobox.Options></Combobox.Options>
+            <Combobox.Options>
+              {filteredManufacturers.length === 0 && query !== "" && (
+                <Combobox.Option
+                  value={query}
+                  className="search-manufacturer__option"
+                >
+                  Create"{query}"
+                </Combobox.Option>
+              )}
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
